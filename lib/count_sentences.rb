ff2bb81696dc4820dde1a72cc361ec94d4756ab4
Brandon Weaver <a7xncob@gmail.com>
2019-11-08 18:58:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
+    return self.end_with?('.')
+  end
+  
+  def question?
     
   end
-
-  def question?
-
-  end
-
+  
   def exclamation?
-
+    
   end
-
+  
   def count_sentences
-
+    
   end
 end
